bb974073c656f8072fb91ce4e7b990d81a257a9f
Arjay Andal <arjayandal93@gmail.com>
2022-06-15 20:31:55 +0800

    to fix: modal function in approve

--- pages/request_page.php
+++ pages/request_page.php
@@ -1,5 +1,7 @@
 <?php element('header1')?>
 <link rel="stylesheet" type="text/css" href="<?php echo SITE_URL ?>/assets/css/card.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <style>
     .btn-side-button{
         width: 250px;
@@ -61,7 +63,7 @@
 					</div>
 				</div>
 				<div class="button mt-2 d-flex flex-row align-items-center">
-					<a href="<?php echo SITE_URL ?>/?page=approve_user&id=<?php echo $row['id']?>" ><button class="btn btn-sm btn-outline-primary" style="margin-right: 20px;">Approve</button></a>
+					<button class="btn btn-sm btn-outline-primary" data-bs-toggle="modal" data-bs-target="#myModal" style="margin-right: 20px;">Approve</button></a>
 					<a href="reject_product.php?id=<?php echo $row['id']?>"><button class="btn btn-sm btn-outline-danger"  style="width: 70px;">Reject</button></a>
 				</div>
 				</div>
@@ -75,4 +77,29 @@
 </div>
 <?php element('footer1')?>
 
+<!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+<!-- END OF MODAL -->
 
